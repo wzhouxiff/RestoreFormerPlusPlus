9484410a7f2f5acb51ed855a1f24c0f7bef81b12
wzhouxiff <wzhoux@mail2.sysu.edu.cn>
2023-08-16 10:19:12 +0800

    align with the code in 30042

--- scripts/metrics/run.sh
+++ scripts/metrics/run.sh
@@ -7,7 +7,12 @@ root='/group/30042/zhouxiawang/checkpoints/RestoreFormer/resutls/WebPhoto-Test'
 out_root='/group/30042/zhouxiawang/checkpoints/RestoreFormer/metrics'
 
 test_name='RestoreFormer++'
+
+test_name='2022-11-16T14-55-40_RestoreFormer++RestoreFormer++_lmdb_gpus8_h4_E62_ROHQD105_seed32'
+test_name='2022-11-21T10-49-14_RestoreFormer++RestoreFormer++_lmdb_gpus8_h4_E62_ROHQD105_En105_lr7_seed44'
 tag='test'
+
+
 
 test_image=$test_name'_'$tag'/restored_faces'
 out_name=$test_name
@@ -19,8 +24,14 @@ CelebAHQ_GT='YOUR_PATH'
 # FID
 python -u scripts/metrics/cal_fid.py \
 $root'/'$test_image \
---fid_stats 'experiments/pretrained_models/inception_FFHQ_512-f7b384ab.pth' \
---save_name $out_root'/'$out_name'_fid.txt' \
+--fid_stats 'experiments/pretrained_models/inception_FFHQ_512_color_gray008_shift02.pth' \
+--save_name $out_root'/'$out_name'_fid_gray008shift02.txt' \
+
+# --fid_stats 'experiments/pretrained_models/inception_FFHQ_512-f7b384ab.pth' \
+# --save_name $out_root'/'$out_name'_fid.txt' \
+
+# --fid_stats 'experiments/pretrained_models/inception_FFHQ_512_color_gray008_shift02.pth' \
+# --save_name $out_root'/'$out_name'_fid_gray008shift02.txt' \
 
 if [ -d $CelebAHQ_GT ]
 then
--- scripts/test_daily.sh
+++ scripts/test_daily.sh
@@ -8,6 +8,14 @@ align_test_path='data/test'
 tag='test'
 
 
+root_path='/group/30042/zhouxiawang/checkpoints/RestoreFormer/release/logs'
+# exp_name='2022-11-16T14-55-40_RestoreFormer++RestoreFormer++_lmdb_gpus8_h4_E62_ROHQD105_seed32'
+# epoch='105'
+
+exp_name='2022-11-21T10-49-14_RestoreFormer++RestoreFormer++_lmdb_gpus8_h4_E62_ROHQD105_En105_lr7_seed44'
+epoch=63
+epoch=109
+
 out_root_path='/group/30042/zhouxiawang/checkpoints/RestoreFormer/resutls/WebPhoto-Test'
 align_test_path='/group/30042/zhouxiawang/data/FaceRestoration/WebPhoto-Test'
 
@@ -19,8 +27,8 @@ fi
 
 CUDA_VISIBLE_DEVICES=7 python -u scripts/test.py \
 --outdir $outdir \
--r $root_path'/'$exp_name'/last.ckpt' \
--c 'configs/'$exp_name'.yaml' \
+-r $root_path'/'$exp_name'/checkpoints/last.ckpt.'$epoch \
+-c 'configs/RestoreFormer++.yaml' \
 --test_path $align_test_path \
 --aligned
 
